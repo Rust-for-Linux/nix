a6a63882b1dfd8c7a811426c3b8cdcf6c71e1ce2
Finn Behrens <me@kloenk.dev>
2021-08-27 19:44:26 +0200

    remove the doc jobset

--- .hydra/declarative-jobsets.nix
+++ .hydra/declarative-jobsets.nix
@@ -64,36 +64,6 @@ let
         };
       };
     };
-    doc = {
-      description = "Build linux rust tree";
-      checkinterval = "60";
-      enabled = "1";
-      nixexprinput = "expr";
-      nixexprpath = "default.nix";
-      schedulingshares = 100;
-      enableemail = false;
-      emailoverride = "";
-      keepnr = 3;
-      hidden = false;
-      type = 0;
-      inputs = {
-        linux = {
-          value = "https://github.com/rust-for-linux/linux rust";
-          type = "git";
-          emailresponsible = false;
-        };
-        nixpkgs = {
-          value = "https://github.com/NixOS/nixpkgs staging-next";
-          type = "git";
-          emailresponsible = false;
-        };
-        expr = {
-          value = "https://github.com/rust-for-linux/nix doc";
-          type = "git";
-          emailresponsible = false;
-        };
-      };
-    };
     kloenk-net = {
       description = "Kloenk's try to provide network apis";
       checkinterval = "60";
