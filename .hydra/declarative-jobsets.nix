503be3bfb570afc8566050aed2155e5374e0c804
Finn Behrens <me@kloenk.dev>
2022-01-15 21:50:09 +0100

    add staging next job

--- .hydra/declarative-jobsets.nix
+++ .hydra/declarative-jobsets.nix
@@ -64,6 +64,36 @@ let
         };
       };
     };
+    staging-next = {
+      description = "Build linux rust tree";
+      checkinterval = "60";
+      enabled = "1";
+      nixexprinput = "expr";
+      nixexprpath = "default.nix";
+      schedulingshares = 50;
+      enableemail = false;
+      emailoverride = "";
+      keepnr = 3;
+      hidden = false;
+      type = 0;
+      inputs = {
+        linux = {
+          value = "https://github.com/rust-for-linux/linux rust";
+          type = "git";
+          emailresponsible = false;
+        };
+        nixpkgs = {
+          value = "https://github.com/NixOS/nixpkgs staging-next";
+          type = "git";
+          emailresponsible = false;
+        };
+        expr = {
+          value = "https://github.com/rust-for-linux/nix main";
+          type = "git";
+          emailresponsible = false;
+        };
+      };
+    };
     kloenk-net = {
       description = "Kloenk's try to provide network apis";
       checkinterval = "60";
